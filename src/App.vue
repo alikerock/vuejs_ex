038ce0c3272944a631c793c29bcc874ecf3c0af8
rock's ezweb <alikerock@gmail.com>
2023-10-24 13:20:34 +0900

    class style binding

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,8 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/template">Template 문법</RouterLink>
         <RouterLink to="/reactivity">Reactivity</RouterLink>
+        <RouterLink to="/computed">computed</RouterLink>
+        <RouterLink to="/binding">Class style Binding</RouterLink>
       </nav>
     </div>
   </header>
--- src/router/index.js
+++ src/router/index.js
@@ -23,6 +23,16 @@ const router = createRouter({
       path: '/reactivity',
       name: 'reactivity',
       component: () => import('../views/ReactivityView.vue')
+    },
+    {
+      path: '/computed',
+      name: 'computed',
+      component: () => import('../views/ComputedView.vue')
+    },
+    {
+      path: '/binding',
+      name: 'binding',
+      component: () => import('../views/BindingView.vue')
     }
   ]
 })
